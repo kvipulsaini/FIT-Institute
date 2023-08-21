744c71e949e0ec5c8b248c0f85411f6a5897f40a
VIPUL SAINI <53830072+kvipulsaini@users.noreply.github.com>
2023-08-22 00:19:12 +0530

    Add files via upload

---
+++ fees.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 21, 2023 at 08:48 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `clickfees_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fees`
+--
+
+CREATE TABLE `fees` (
+  `id` int(30) NOT NULL,
+  `course_id` int(30) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `amount` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `fees`
+--
+
+INSERT INTO `fees` (`id`, `course_id`, `description`, `amount`) VALUES
+(1, 1, 'Tution Fee', 5000),
+(2, 1, 'Uniform Fee', 1200),
+(3, 2, 'Uniform Fee', 5000),
+(4, 2, 'Tuition Fee', 56000),
+(5, 2, 'Maintaince Fee', 600),
+(7, 3, 'Tution Test', 90000),
+(8, 3, 'Transport Fees', 14000),
+(9, 4, 'Tution Fee', 150000),
+(10, 4, 'Transport', 8000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `fees`
+--
+ALTER TABLE `fees`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `fees`
+--
+ALTER TABLE `fees`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
